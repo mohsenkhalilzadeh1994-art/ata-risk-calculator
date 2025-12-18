89ad6151b426e9f657ab68169dc639f0d0b61a44
mohsenKhalilzadeh-art <mohsen.khalilzadeh1994@gmail.com>
2025-12-18 23:38:49 +0300

    remove top margin from footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "ATA 2025 Risk Calculator",
-  description: "Generated at Ege University, Department of Nuclear Medicine",
+  description: "Educational and clinical decision-support use only.",
   icons: {
     icon: "/favicon.png",
   },
@@ -31,6 +31,27 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <footer className=" border-zinc-200 bg-zinc-50">
+          <div className="mx-auto max-w-6xl px-4 py-6 text-sm text-zinc-600 flex flex-col gap-2 md:flex-row md:items-center md:justify-between">
+            <div>
+              <strong className="text-zinc-800">
+                ATA 2025 Risk Calculator
+              </strong>{" "}
+              Developed at Ege University, Department of Nuclear Medicine by M.
+              Khalilzadeh.
+            </div>
+
+            <div>
+              Contact:{" "}
+              <a
+                href="mailto:mohsenkhalilzadeh1994@gmail.com"
+                className="text-indigo-600 hover:underline"
+              >
+                atariskcalculator@gmail.com
+              </a>
+            </div>
+          </div>
+        </footer>
       </body>
     </html>
   );
